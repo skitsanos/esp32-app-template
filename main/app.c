2bf7849c6b782ffa3fbc3450f092d6fb607f62a4
Skitsanos <info@skitsanos.com>
2019-01-29 12:04:58 +0200

    updates for the app template

--- main/app.c
+++ main/app.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <console_colors.h>
 
 void app_main()
 {
-    printf("Hello from ESP32!\n");
+    console_set_color(CONSOLE_COLORS_RESET);
+
+    printf("\033[46;1m ╔═══════════════════════════════════╗\033[0m\n");
+    printf("\033[46;1m ║             hello world           ║\033[0m\n");
+    printf("\033[46;1m ╚═══════════════════════════════════╝\033[0m\n");
 }
---
+++ main/app_config.h
@@ -0,0 +1,4 @@
+#define TAG "my-app"
+
+#define ap_ssid "ESP32-node"
+#define ap_password "password12345678"
